ff6a423d7e16c797da1ce11d1946c6c568167e85
nsp5488 <nsp5488@g.rit.edu>
2024-12-28 06:35:20 -0500

    implemented  everything up to AWS

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +35,53 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	videoData, err := cfg.db.GetVideo(videoID)
+	headerFile, err := header.Open()
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse header", err)
+	}
+	defer headerFile.Close()
+
+	headerData, err := io.ReadAll(headerFile)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse header", err)
+	}
+	contentType := http.DetectContentType(headerData)
+
+	mediaType, _, err := mime.ParseMediaType(contentType)
+
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Unable to process file", err)
+	}
+	contentTypes := strings.Split(mediaType, "/")
+
+	randomData := make([]byte, 32)
+	rand.Read(randomData)
+	fileName := base64.RawURLEncoding.EncodeToString(randomData)
+
+	outFile, err := os.Create(filepath.Join(cfg.assetsRoot, fileName) + "." + contentTypes[1])
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to save thumbnail", err)
+	}
+	defer outFile.Close()
+	io.Copy(outFile, file)
+
+	thumbUrl := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, fileName, contentTypes[1])
+
+	videoData.ThumbnailURL = &thumbUrl
+	err = cfg.db.UpdateVideo(videoData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update video", err)
+	}
+	respondWithJSON(w, http.StatusOK, videoData)
 }
